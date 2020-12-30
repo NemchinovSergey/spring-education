37f8f18a0960c7b3cd0836aa087f8b8fa25a4b9b
SNemchinov <googoodemon@gmail.com>
2020-12-30 16:21:31 +0700

    client authentication (mutual TLS)

---
+++ mutual-tls/src/main/java/com/example/demo/config/SecureRestTemplateConfig.java
@@ -0,0 +1,108 @@
+package com.example.demo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.Arrays;
+
+@Slf4j
+@Configuration
+public class SecureRestTemplateConfig {
+
+    public static final String SUN_X_509 = "SunX509";
+
+    @Value("${secure-rest.keyStore}")
+    String keyStore;
+
+    @Value("${secure-rest.keyStorePassword}")
+    String keyStorePassword;
+
+    @Value("${secure-rest.keyPassword}")
+    String keyPassword;
+
+    /**
+     * URL location, typically with file:// scheme, of a CA trust store file in JKS format.
+     */
+    @Value("${secure-rest.trustStore}")
+    String trustStore;
+
+    /**
+     * The store password of the given trust store.
+     */
+    @Value("${secure-rest.trustStorePassword}")
+    char[] trustStorePassword;
+
+    /**
+     * One of the SSLContext algorithms listed at
+     * https://docs.oracle.com/javase/8/docs/technotes/guides/security/StandardNames.html#SSLContext .
+     */
+    @Value("${secure-rest.protocol}")
+    String protocol = "TLSv1.2";
+
+    @Bean
+    public RestTemplate restTemplate() {
+        try {
+            File keyStoreFile = new DefaultResourceLoader(null).getResource(keyStore).getFile();
+
+            SSLContext sslContext = SSLContextBuilder.create()
+                    .setKeyStoreType(KeyStore.getDefaultType()) // JKS or p12
+                    .setKeyManagerFactoryAlgorithm(SUN_X_509)
+                    .setTrustManagerFactoryAlgorithm(SUN_X_509)
+                    .loadKeyMaterial(keyStoreFile, keyStorePassword.toCharArray(), keyStorePassword.toCharArray())
+                    .loadTrustMaterial(new TrustAllStrategy())
+                    .setProtocol(protocol)
+                    .build();
+
+            LayeredConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
+
+            final HttpClient httpClient = HttpClientBuilder.create()
+                    .setSSLSocketFactory(sslSocketFactory)
+                    .build();
+
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
+            return restTemplate;
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to setup client SSL context", e);
+        } finally {
+            // it's a good security practice to zero out passwords,
+            // which is why they're char[]
+            Arrays.fill(trustStorePassword, (char) 0);
+        }
+    }
+
+    /**
+     * <pre>
+     * KeyStore keyStore = loadKeyStore("/keystore.jks", keyStorePassword);
+     * Key key = keyStore.getKey("keyAlias", keyPassword);
+     * log.info("Key: {}", key);
+     * </pre>
+     */
+    private KeyStore loadKeyStore(String keystorePath, String keystorePassword) throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException {
+        try(InputStream keystoreInputStream = SecureRestTemplateConfig.class.getResourceAsStream(keystorePath)) {
+            KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keystore.load(keystoreInputStream, keystorePassword.toCharArray());
+            return keystore;
+        }
+    }
+}
--- mutual-tls/src/main/java/com/example/demo/controller/TestController.java
+++ mutual-tls/src/main/java/com/example/demo/controller/TestController.java
@@ -2,9 +2,16 @@ package com.example.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
@@ -14,10 +21,23 @@ public class TestController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("/test")
-    public void test() {
-        ResponseEntity<String> resp = restTemplate.getForEntity("https://client.badssl.com/", String.class);
-        log.info("Resp: {}", resp.getBody());
+    @RequestMapping(value = "/test", params = "url")
+    public ResponseEntity<String> test(@RequestParam String url, HttpServletRequest request) {
+        try {
+            ResponseEntity<String> resp = restTemplate.exchange(
+                    url,
+                    HttpMethod.valueOf(request.getMethod()),
+                    HttpEntity.EMPTY,
+                    String.class);
+            log.info("Resp: {}", resp.getBody());
+            return resp;
+        } catch (HttpClientErrorException e) {
+            log.error("HttpClientErrorException: {}", e.getMessage(), e);
+            return ResponseEntity
+                    .status(e.getStatusCode())
+                    .headers(e.getResponseHeaders())
+                    .body(e.getResponseBodyAsString());
+        }
     }
 
 }
